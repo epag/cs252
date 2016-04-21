55948f2fc0828e4a8b4270aa2a90e5e13635be63
Wyatt Dahlenburg <wdahlenb@purdue.edu>
2016-04-21 19:40:36 +0000

    Logs

--- public/display.php
+++ public/display.php
@@ -1,10 +1,17 @@
 <?php
 
 $con=mysql_connect("75.126.155.153","user17809","PWHK2WEyIvEo");  
+if (!$con)
+{
+    die('Could not connect: ' . mysql_error());
+    console.log("Could not connect to sql server");
+}   
 
 mysql_select_db("SQLDB",$con);
 
 $result=mysql_query("SELECT dictionary FROM USER17809.words ORDER BY RAND() LIMIT 1",$con);
+
+console.log("Word is " + $request);
 
 $data = mysql_fetch_row($result)
 echo "<h1>$data</h1>";
